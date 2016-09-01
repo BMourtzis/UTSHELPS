6b502052a7415abb5f88a0bc43520989b311d859
BMourtzis <vasilis.mourtzis@outlook.com>
2016-09-01 14:16:40 +1000

    Initial commit with a sample android project

---
+++ UTSHelpsApp/app/src/main/java/com/example/vm/utshelpsapp/Repo.java
@@ -0,0 +1,11 @@
+package com.example.vm.utshelpsapp;
+
+/**
+ * Created by Vasil on 1/9/2016.
+ */
+public class Repo {
+    int id;
+    String name;
+    String full_name;
+    String html_url;
+}
---
+++ UTSHelpsApp/app/src/main/java/com/example/vm/utshelpsapp/iStudent.java
@@ -0,0 +1,8 @@
+package com.example.vm.utshelpsapp;
+
+/**
+ * Created by Vasil on 1/9/2016.
+ */
+public interface iStudent {
+
+}
